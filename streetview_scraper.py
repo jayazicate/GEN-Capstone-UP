8eb0630a9bb88ce73607a3ea38472c787af194ee
Adam Mercer <mercer20@up.edu>
2019-10-02 13:00:27 -0700

    Google Street View Scraper
    Get a street view picture of an address using Google Maps Street View Satics API

---
+++ streetview_scraper.py
@@ -0,0 +1,13 @@
+import os, urllib.parse, urllib.request
+
+SAVE_LOCATION = r"/Users/adammercer/Desktop" # location on computer to store image at
+API_KEY = '&key=' + os.environ.get('STREET_VIEW_API_KEY') # retrieve api key as an environment variable
+TEST_ADDRESS = '5000 NE Fourth Plain Blvd #E-104, Vancouver, WA 98661' # a massage parlor in Washington
+
+def getStreetView(address, saveLocation):
+    base = 'https://maps.googleapis.com/maps/api/streetview?size=800x800&location='
+    url = base + urllib.parse.quote_plus(address) + API_KEY # added url encoding
+    fi = address + '.jpg' # save file name
+    urllib.request.urlretrieve(url, os.path.join(saveLocation, fi))
+
+getStreetView(TEST_ADDRESS, SAVE_LOCATION)
