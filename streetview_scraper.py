b01d61ff7adf24b6e4894c8d4d194ac9d7e3b781
Adam Mercer <mercer20@up.edu>
2019-10-23 16:32:00 -0700

    XML to CSV
    Add script to convert labels in XML format to CSV format

--- streetview_scraper.py
+++ streetview_scraper.py
@@ -2,7 +2,7 @@ import os, urllib.parse, urllib.request
 
 SAVE_LOCATION = r"/Users/adammercer/Desktop" # location on computer to store image at
 API_KEY = '&key=' + os.environ.get('STREET_VIEW_API_KEY') # retrieve api key as an environment variable
-TEST_ADDRESS = '5000 NE Fourth Plain Blvd #E-104, Vancouver, WA 98661' # a massage parlor in Washington
+TEST_ADDRESS = '820 W 7TH AVE, SPOKANE, WA, 99204, UNITED STATES' # a massage parlor in Washington
 
 def getStreetView(address, saveLocation):
     base = 'https://maps.googleapis.com/maps/api/streetview?size=800x800&location='
