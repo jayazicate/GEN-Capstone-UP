083d0f653312d46568627579f320d7bc569b281d
Adam Mercer <mercer20@up.edu>
2019-12-05 19:52:59 -0800

    Revert "Image similarity"
    This reverts commit 6cf736b86d73b6eadd4c96ae40f827827bf69e24.

--- streetview_scraper.py
+++ streetview_scraper.py
@@ -1,23 +1,13 @@
-import os
-import urllib.parse, urllib.request
-import csv
+import os, urllib.parse, urllib.request
 
-SAVE_LOCATION = 'streetview/' # location to store image at
+SAVE_LOCATION = r"/Users/adammercer/Desktop" # location on computer to store image at
 API_KEY = '&key=' + os.environ.get('STREET_VIEW_API_KEY') # retrieve api key as an environment variable
-ADDRESS_FILE = 'mb_final.csv'
+TEST_ADDRESS = '820 W 7TH AVE, SPOKANE, WA, 99204, UNITED STATES' # a massage parlor in Washington
 
-def get_street_view(address, save_file_path):
+def getStreetView(address, saveLocation):
     base = 'https://maps.googleapis.com/maps/api/streetview?size=800x800&location='
     url = base + urllib.parse.quote_plus(address) + API_KEY # added url encoding
-    urllib.request.urlretrieve(url, save_file_path)
+    fi = address + '.jpg' # save file name
+    urllib.request.urlretrieve(url, os.path.join(saveLocation, fi))
 
-with open(ADDRESS_FILE) as csv_file:
-    reader = csv.reader(csv_file, delimiter=',')
-    next(reader) # skip header line
-    i = 0
-    for row in reader:
-        if i >= 100:
-            break
-        address = row[31]
-        get_street_view(address, SAVE_LOCATION + address + '.jpg')
-        i += 1
+getStreetView(TEST_ADDRESS, SAVE_LOCATION)
